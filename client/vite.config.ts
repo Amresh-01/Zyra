2c05ef6b35dc361030eee203c3244740eaf95b9f
Amresh-01 <amreshchaurasiya373@gmail.com>
2026-06-01 22:24:18 +0530

    Add tailwind css

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(),],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
   },
 });
+
